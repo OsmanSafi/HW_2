3dbe5b804fa2e287d2c4da25b32fb731a0b4c3cd
Donovan <donovan.pinchot1@gmail.com>
2024-11-27 17:35:20 -0800

    Fixed issue related to Navigation features

--- Desktop/Personal Projects/HW2/app/src/main/java/com/CSUF544/hw2/MainActivity.java
+++ Desktop/Personal Projects/HW2/app/src/main/java/com/CSUF544/hw2/MainActivity.java
@@ -4,10 +4,6 @@ import android.os.Bundle;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 import com.CSUF544.hw2.databinding.ActivityMainBinding;
 
 import android.view.Menu;
@@ -15,7 +11,6 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
 private ActivityMainBinding binding;
 
     @Override
@@ -26,10 +21,6 @@ private ActivityMainBinding binding;
      setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,10 +53,4 @@ private ActivityMainBinding binding;
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
